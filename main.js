78acd00ced2f6a2a26d8d73edfc04528768884e7
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-10-04 12:39:39 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal");
+  modal.classList.add("hidden");
 
+  const likeButtons = document.querySelectorAll(".like-glyph");
 
+  likeButtons.forEach((heart) => {
+    heart.addEventListener("click", function () {
+      const isFull = heart.classList.contains("activated-heart");
+      if (isFull) {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      } else {
+        heart.textContent = FULL_HEART;
+        heart.classList.add("activated-heart");
+
+        mimicServerCall()
+          .then((resp) => {
+            changeHeartFill(resp, e);
+          })
+          .catch((error) => {
+            modal.innerHTML = error;
+          });
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
